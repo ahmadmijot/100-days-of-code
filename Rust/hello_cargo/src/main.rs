2b3dbaaceacfb8cd29f654f151be771b655eb1aa
Max Mijot <ahmad.mijot@gmail.com>
2019-09-28 01:59:25 +0800

    create hello_cargo

---
+++ Rust/hello_cargo/src/main.rs
@@ -0,0 +1,11 @@
+/*
+This tutorial taken from https://www.simonpbriggs.co.uk/rust.pdf
+Create project with Cago, at terminal: cargo new projectName
+
+build project: cargo build
+execute file: ./target/debug/hello_cargo
+
+*/
+fn main() {
+    println!("Hello, world! I'm Rustacean!");
+}
---
+++ Rust/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+C:\Users\Max\ Mijot\Documents\GitHub\100-days-of-code\Rust\hello_cargo\target\debug\hello_cargo.exe: C:\Users\Max\ Mijot\Documents\GitHub\100-days-of-code\Rust\hello_cargo\src\main.rs
