7baef53da1db9e811e9304b9808086f0ad1b239b
Max Mijot <ahmad.mijot@gmail.com>
2019-10-02 14:21:40 +0800

    create new project

---
+++ Rust/branch/src/main.rs
@@ -0,0 +1,38 @@
+/*
+Rust flows
+Rust loop: loop, while, for
+*/
+
+fn main() {
+    flow_func();
+    loop_func();
+}
+
+fn flow_func() {
+    let condition = true;
+    let number = if condition {
+        "six"
+    } else {
+        "five"
+    };
+
+    println!("The value of number is: {}", number);
+}
+
+fn loop_func() {
+    // loop until explicitely tell to stop
+
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+        println!("{}",counter);
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    println!("The result is {}", result);
+}
+
