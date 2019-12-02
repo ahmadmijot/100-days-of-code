9667196ae2d7d962c53de2cb326f7e8d89b64fd7
Max Mijot <ahmad.mijot@gmail.com>
2019-12-03 00:07:09 +0800

    start using for loop using jinja2

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -4,5 +4,15 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Max'}
-    return render_template('index.html', title='Home', user=user)
+    user = {'username': 'Max'} #mock user
+    posts = [                   # mock post
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day today!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The movie was so good!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
